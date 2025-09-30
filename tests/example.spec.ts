0de3a5ad629d2c6d19ebf93dec1e26cb191028cd
Matthew Nelson <matthewbuzz@gmail.com>
2025-09-30 16:08:01 -0600

    Set up Playwright

---
+++ tests/example.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from 'playwright-test-coverage';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await expect(page).toHaveTitle('JWT Pizza');
+});
+
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
