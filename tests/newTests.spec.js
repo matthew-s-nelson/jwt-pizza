6ab1c499b6b5b4323fcf149d54595c038c786f93
Matthew Nelson <matthewbuzz@gmail.com>
2025-10-13 13:49:07 -0600

    Mock out API calls for update user test

--- tests/newTests.spec.js
+++ tests/newTests.spec.js
@@ -1,17 +1,54 @@
 import { test, expect } from 'playwright-test-coverage';
+import { login } from './testingUtils';
 
 test('updateUser', async ({ page }) => {
-  const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
-  await page.goto('/');
-  await page.getByRole('link', { name: 'Register' }).click();
-  await page.getByRole('textbox', { name: 'Full name' }).fill('pizza diner');
-  await page.getByRole('textbox', { name: 'Email address' }).fill(email);
-  await page.getByRole('textbox', { name: 'Password' }).fill('diner');
-  await page.getByRole('button', { name: 'Register' }).click();
+  login(page);
+  
+  await page.route('*/**/api/order', async (route) => {
+    const orderRes = {
+      order: {
+        items: [
+          { menuId: 1, description: 'Veggie', price: 0.0038 },
+          { menuId: 2, description: 'Pepperoni', price: 0.0042 },
+        ],
+        storeId: '4',
+        franchiseId: 2,
+        id: 23,
+      },
+      jwt: 'eyJpYXQ',
+    };
+    expect(route.request().method()).toBe('GET');
+    await route.fulfill({ json: orderRes });
+  });
 
-  await page.getByRole('link', { name: 'pd' }).click();
 
-  await expect(page.getByRole('main')).toContainText('pizza diner');
+  await page.route('*/**/api/user/**', async (route) => {
+    const userId = route.request().url().split('/').pop();
+    const userReq = {
+      "id": 3,
+      "name": "pizza dinerx",
+      "email": "d@jwt.com",
+      "roles": [
+        {
+          "role": "admin"
+        }
+      ]
+    };
+    const userRes = {
+      user: { id: userId, name: 'diner 1', email: 'a@jwt.com', roles: [{ role: 'diner' }] },
+    };
+    expect(route.request().method()).toBe('PUT');
+    expect(route.request().postDataJSON()).toMatchObject(userReq);
+    await route.fulfill({ json: userRes });
+  });
+  await page.getByRole('link', { name: 'KC' }).click();
+
+  await page.route('*/**/api/auth', async (route) => {
+    await route.fulfill({});
+  });
+
+
+  await expect(page.getByRole('main')).toContainText('Kai Chen');
 
   await page.getByRole('button', { name: 'Edit' }).click();
   await expect(page.locator('h3')).toContainText('Edit user');
@@ -21,15 +58,4 @@ test('updateUser', async ({ page }) => {
   await page.waitForSelector('[role="dialog"].hidden', { state: 'attached' });
 
   await expect(page.getByRole('main')).toContainText('pizza dinerx');
-
-  await page.getByRole('link', { name: 'Logout' }).click();
-    await page.getByRole('link', { name: 'Login' }).click();
-
-    await page.getByRole('textbox', { name: 'Email address' }).fill(email);
-    await page.getByRole('textbox', { name: 'Password' }).fill('diner');
-    await page.getByRole('button', { name: 'Login' }).click();
-
-    await page.getByRole('link', { name: 'pd' }).click();
-
-    await expect(page.getByRole('main')).toContainText('pizza dinerx');
 });
